1b0048907ad9d1528c0c14d0213d40b870719b09
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2024-06-05 19:55:52 +0530

    overall models in single file added. later need to separate out

--- Models/User.cs
+++ Models/User.cs
@@ -2,7 +2,7 @@
 
 namespace AuthDemo.Models
 {
-    public class User
+   /* public class User
     {
         [Key]
         public int Id { get; set; }
@@ -12,16 +12,8 @@ namespace AuthDemo.Models
         public string PasswordHash { get; set; }
         public int? FailedLoginAttempts { get; set; } 
         public DateTime? LockoutEnd { get; set; }
-    }
+    }*/
 
-    public class Asset
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string AssetName { get; set; }
-        public string Description { get; set; }
-        public int UserId { get; set; }
-
-    }
+   
 
 }
